9dde3b902b201f7f5de6d3ba68dd72e5d2326d53
iwasmateo <iwasmateo@gmail.com>
2025-10-10 20:28:32 +0200

    Doing a lot of tracking and shit

--- gen-game.py
+++ gen-game.py
@@ -2,16 +2,38 @@ import pygame
 import math
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("My Game")
+pygame.display.set_caption("Walking Ant")
 clock = pygame.time.Clock()
 running = True
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
 def getDistance(object1, object2):
     x1, y1 = object1.x, object1.y
     x2, y2 = object2.x, object2.y
     return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+def getMiddlex(object1, object2):
+    x1 = object1.x
+    x2 = object2.x
+    return ((x1 + x2) / 2)
+def getMiddley(object1, object2):
+    y1 = object1.y
+    y2 = object2.y
+    return ((y1 + y2) / 2)
+
+class Middlepoint:
+    def __init__(self, object1, object2):
+        self.object1 = object1
+        self.object2 = object2
+    @property
+    def x(self):
+        return(self.object1.x+ self.object2.x)/2
+    @property
+    def y(self):
+        return(self.object1.y+ self.object2.y)/2
+
 class Dot:
     def __init__(self, x, y, width, height, color, speed):
         self.x = x
@@ -21,9 +43,9 @@ class Dot:
         self.color = color
         self.speed = speed
     def draw(self, surface):
-        pygame.draw.rect(surface, self.color, (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(surface, self.color, (self.x-self.width/2, self.y-self.height/2, self.width, self.height))
 
-    def move(self, keys):
+    def arrow_move(self, keys):
         if keys[pygame.K_LEFT]:
             self.x -= self.speed
         if keys[pygame.K_RIGHT]:
@@ -32,9 +54,18 @@ class Dot:
             self.y -= self.speed
         if keys[pygame.K_DOWN]:
             self.y += self.speed
+    def wasd_move(self, keys):
+        if keys[pygame.K_a]:
+            self.x -= self.speed
+        if keys[pygame.K_d]:
+            self.x += self.speed
+        if keys[pygame.K_w]:
+            self.y -= self.speed
+        if keys[pygame.K_s]:
+            self.y += self.speed
     def pos(self):
         print("Position:", self.x, self.y)
-    def moveTowards(self, target):
+    def moveTowards(self, target, move_speed):
         dx = target.x - self.x
         dy = target.y - self.y
         dist = math.hypot(dx, dy)
@@ -42,11 +73,13 @@ class Dot:
             dx /= dist
             dy /= dist
 
-            self.x += dx * self.speed
-            self.y += dy * self.speed
+            self.x += dx * move_speed
+            self.y += dy * move_speed
 
-Body = Dot(100, 100, 40, 40, RED, 1)
-Ankle = Dot(150, 150, 20, 20, BLUE, 5)
+Body = Dot(100, 100, 40, 40, BLACK, 1)
+Ankle1 = Dot(150, 150, 20, 20, RED, 5)
+Ankle2 = Dot(50, 50, 20, 20, GREEN, 5)
+Anklemid = Middlepoint(Ankle1, Ankle2)
 while running:
     for i in pygame.event.get():
         if i.type==pygame.QUIT:
@@ -55,18 +88,24 @@ while running:
     
     keys = pygame.key.get_pressed()
     Body.draw(screen)
-    Ankle.draw(screen)
-    
-    # print("Distance:", getDistance(Body, Ankle))
-    if getDistance(Body, Ankle) < 100:
-        Ankle.move(keys)
+    Ankle1.draw(screen)
+    Ankle2.draw(screen)
+    # print("Distance:", getDistance(Body, Ankle1))
+    if getDistance(Body, Ankle1) < 75: # and getDistance(Body, Ankle1) > 20:
+        Ankle1.arrow_move(keys)
     else:
-        while getDistance(Body, Ankle) > 50:
-            Body.moveTowards(Ankle)
-
-
-
+        pass
+    if getDistance(Body, Ankle2) < 75: # and getDistance(Body, Ankle2) > 20:
+        Ankle2.wasd_move(keys)
+    else:
+        pass
+    if getDistance(Body, Anklemid) > 5:
+        Body.moveTowards(Anklemid, 1)
+    pygame.draw.line(screen, (0,0,0), (Body.x, Body.y), (Ankle1.x, Ankle1.y), 3)
+    pygame.draw.line(screen, (0,0,0), (Body.x, Body.y), (Ankle2.x, Ankle2.y), 3)
+    pygame.draw.line(screen, BLUE, (Ankle1.x, Ankle1.y), (Ankle2.x, Ankle2.y), 3)    
     pygame.display.flip()
-    clock.tick(30)
+    clock.tick(60)
+    print(Anklemid.x,Anklemid.y)
 
 pygame.quit()
